fd08dd8eadea8967ae7054233c90d47c7c74fd44
TwistenTiger <twistentiger@gmail.com>
2024-09-02 15:18:17 +0800

    40 mins into the project
    Exporting files created.
    Refactor of code and methods.

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -2,6 +2,7 @@ import 'dart:js_interop';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/user.dart';
 import 'package:user_repository/src/user_repo.dart';
 
 class FirebaseUserRepo implements UserRepository {
@@ -19,7 +20,7 @@ class FirebaseUserRepo implements UserRepository {
   }
 
   @override
-  Future<void> setUserData(user) {
+  Future<void> setUserData(MyUser user) {
     // TODO: implement setUserData
     throw UnimplementedError();
   }
@@ -31,12 +32,13 @@ class FirebaseUserRepo implements UserRepository {
   }
 
   @override
-  Future<dynamic> signUp(myUser, String password) {
+  Future<MyUser> signUp(MyUser myUser, String password) {
     // TODO: implement signUp
     throw UnimplementedError();
   }
 
   @override
   // TODO: implement user
-  Stream<dynamic> get user => throw UnimplementedError();
+  Stream<MyUser?> get user => throw UnimplementedError();
+  
 }
