3695e4baac1b4dc2ca461eb37506c69c08a29b30
TwistenTiger <twistentiger@gmail.com>
2024-09-03 19:27:21 +0800

    Fixed build errors and updated packages.
    pub get and pub outdated and all other commands are responding to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pizza_app/simple_bloc_observer.dart';
+import 'package:user_repository/src/firebase_user_repo.dart';
 
 import 'app.dart';
 
